5eae163cd5a7d48586e05f3ed6948402aa708e74
alexisFerrandof22005832 <123471555+alexisFerrandof22005832@users.noreply.github.com>
2023-12-09 02:24:04 +0100

    Delete Zoo_Fantastique_FERRANDO-SOLLE directory

--- Zoo_Fantastique_FERRANDO-SOLLE/src/main/java/org/example/interfaces/AquaticCreature.java
+++
@@ -1,9 +0,0 @@
-// Package contenant l'interface AquaticCreature
-package org.example.interfaces;
-
-// Déclaration de l'interface AquaticCreature
-public interface AquaticCreature {
-
-    // Signature de la méthode swim() à implémenter
-    String swim();
-}
--- Zoo_Fantastique_FERRANDO-SOLLE/src/main/java/org/example/interfaces/FlyingCreature.java
+++
@@ -1,9 +0,0 @@
-// Package contenant l'interface FlyingCreature
-package org.example.interfaces;
-
-// Déclaration de l'interface FlyingCreature
-public interface FlyingCreature {
-
-    // Signature de la méthode fly() à implémenter
-    String fly();
-}
